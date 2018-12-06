2620ff6b74df49f67696bdc5c1367b02f616b29b
Shawn Beaton <beat0154@algonquinlive.com>
2018-12-05 22:48:21 -0500

    update4

---
+++ js/hoverblur.js
@@ -0,0 +1,8 @@
+function removeBlur() {    
+    var element = document.getElementById("blur");
+    element.classList.remove("overlayblur");
+}
+function removeBlur2() {    
+    var element = document.getElementById("blur2");
+    element.classList.remove("overlayblur");
+}
